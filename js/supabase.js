e5bb1c93ed8751dbe7bc3f4ce39d4438961d4baa
AldoBimantara28 <thinkaboutzuu@gmail.com>
2024-12-22 03:27:48 +0700

    Resolve merge conflictss

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,4 @@
- 
+const SUPABASE_URL = 'https://itwcgeclpgsvccgcncii.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0d2NnZWNscGdzdmNjZ2NuY2lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4MTIyNTQsImV4cCI6MjA1MDM4ODI1NH0.9Q0Kvzth3OJElVYY6y09hCByPDFVZuqyfo8Rv4zaAr8';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
